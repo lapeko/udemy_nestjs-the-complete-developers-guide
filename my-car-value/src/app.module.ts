f533c01d9a20577927a60f1bbcba1c79461b7f50
lapeko <lapeko88@gmail.com>
2023-07-26 09:50:04 +0200

    init my-car-value project and add db

---
+++ my-car-value/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
