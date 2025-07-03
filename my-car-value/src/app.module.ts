ee3f57b9db049e7219a19c76db6de9b0e3f6014e
lapeko <lapeko88@gmail.com>
2025-07-03 22:57:58 +0200

    use .env configs

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -6,14 +6,24 @@ import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'sqlite',
+        database: `${config.get('SQLITE_DATABASE_NAME')}`,
+        entities: [User, Report],
+        synchronize: true,
+      }),
     }),
     UsersModule,
     ReportsModule,
