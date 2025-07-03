3755253bd55cd7972d909b23cfa4f12389ad1e19
lapeko <lapeko88@gmail.com>
2025-07-03 18:54:31 +0200

    apply whitelist

--- my-car-value/src/main.ts
+++ my-car-value/src/main.ts
@@ -6,7 +6,7 @@ const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieSession({ keys: ['asdasdasd'] }));
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
