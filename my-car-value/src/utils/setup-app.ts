72f7f3ecaff0ed8626c79b70697c0f9facb8668c
lapeko <lapeko88@gmail.com>
2025-07-03 22:58:08 +0200

    fix import

--- my-car-value/src/utils/setup-app.ts
+++ my-car-value/src/utils/setup-app.ts
@@ -1,5 +1,5 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import cookieSession from 'cookie-session';
+const cookieSession = require('cookie-session');
 
 export const setupApp = (app: INestApplication) => {
   app.use(cookieSession({ keys: ['asdasdasd'] }));
