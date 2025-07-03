abc379e7ad07b3de3297749e9e58682de9010ab9
lapeko <lapeko88@gmail.com>
2025-07-03 18:49:01 +0200

    return forgotten module

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
 import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from './users/users.module';
       synchronize: true,
     }),
     UsersModule,
+    ReportsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
