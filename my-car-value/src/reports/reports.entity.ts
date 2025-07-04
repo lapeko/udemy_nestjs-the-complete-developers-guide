51765569996fab8be8b792cceeff1aae9909d7da
lapeko <lapeko88@gmail.com>
2025-07-04 13:43:45 +0200

    update reports entity

--- my-car-value/src/reports/reports.entity.ts
+++ my-car-value/src/reports/reports.entity.ts
@@ -7,4 +7,22 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  maker: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
 }
