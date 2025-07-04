fbbf92129761f67670e34036a70ac12ca70438d7
lapeko <lapeko88@gmail.com>
2025-07-04 14:00:29 +0200

    implement basic report service, controller and dto

---
+++ my-car-value/src/reports/dtos/create-report-dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+const currentYear = new Date().getFullYear();
+
+export class CreateReportDto {
+  @IsString()
+  maker: string;
+
+  @IsString()
+  model: string;
+
+  @IsNumber()
+  @Min(currentYear - 100)
+  @Max(currentYear)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Min(1e6)
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Min(1e6)
+  price: number;
+}
